ae7acacd69357b4818000af44571823458d26f38
Deepak singh <studydipu@gmail.com>
2025-03-03 23:25:30 +0530

    ConstructorOverloading

--- OOPSinJAVA/constructorOverloading.java
+++ OOPSinJAVA/constructorOverloading.java
@@ -3,13 +3,11 @@ package OOPSinJAVA;
 public class constructorOverloading {
     constructorOverloading(){
         System.out.println("Empty constructor");
-
     }
     constructorOverloading(String name){
         String fullName=name;
         System.out.println(fullName);
     }
-
     public static void main(String[] args) {
          new constructorOverloading();
          new constructorOverloading("Deepak");
